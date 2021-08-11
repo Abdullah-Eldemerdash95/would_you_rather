914b4bb07eddd5e8213440e57b5da0639edc9c17
bodes95 <abdullah_eldemerdash@hotmail.com>
2021-08-11 06:22:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { handleInitialData } from './actions/shared'
+import { connect } from 'react-redux'
+import Home from "./components/Home";
+import Poll from "./components/Poll";
+import PollCreated from "./components/PollCreated";
+import Page404 from "./components/Page404";
+import Login from "./components/Login";
+import LeaderBoard from "./components/LeaderBoard";
+import { BrowserRouter as Router, Route,Switch ,Redirect} from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
 
-function App() {
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData()
+  }
+ render() {
+   console.log(this.props.authedUser)
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route exact path="/" name="Login Page" render={(props) => <Login {...props} />}/>
+       <PrivateRoute exact path={`/Home/:${this.props.authedUser}`}  component={Home} />
+       <PrivateRoute exact path={`/newpoll/:${this.props.authedUser}`}  component={Poll} />
+       <PrivateRoute exact path={`/createdpoll/:${this.props.authedUser}`} component={PollCreated}  />
+       <PrivateRoute exact path={`/leaderboard/:${this.props.authedUser}`} component={LeaderBoard}  />
+       <PrivateRoute component={Page404} /><Redirect to="/404" />
+       </Switch>
     </div>
+    </Router>
   );
 }
+}
+function mapStateToProps (store) {
+  return {
+    authedUser: store.authedUser
+  }
+}
 
-export default App;
+
+export default connect(mapStateToProps,{handleInitialData})(App);
+
+
+/*
+<Route path='/' exact component={Login} />
+       <Route path={`/Home/:${this.props.authedUser}`}  component={Home} />
+       <Route path={`/newpoll/:${this.props.authedUser}`} exact component={Poll} />
+       <Route path={`/createdpoll/:${this.props.authedUser}`} exact component={PollCreated} />
+       <Route path={`/leaderboard/:${this.props.authedUser}`} exact component={LeaderBoard} />
+       <Route component={Page404} />
+       <Route exact path="/" render={(props) => <Login {...props} />}
+          />
+          <PrivateRoute path="/"  component={Login} />
+           */
